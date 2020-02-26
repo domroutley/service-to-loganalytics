9d4a14ece814ea0e3141b3deeeacb0f0df8fee74
dom routley <dom@DOG00188.local>
2020-02-26 11:37:49 +0000

    add exception handling around subprocess

--- main.py
+++ main.py
@@ -4,10 +4,22 @@ import datetime
 def main(services_i_care_about):
     line_to_write = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     for service in services_i_care_about:
-        status = subprocess.check_output(['systemctl', 'is-active', service])
-        line_to_write = line_to_write + ' ' + service + ' ' + status
+        try:
+            # Get the status of the service
+            # Strip newline character from the end of the line
+            status = subprocess.check_output(['systemctl', 'is-active', service])[:-1]
+        except subprocess.CalledProcessError as e:
+            # Strip the newline from end of the line
+            status = e.output[:-1]
+        except Exception:
+            # Log somthing vaguely useful if we do not know whats gone wrong
+            status = 'unknown_exception_in_logger'
+        finally:
+            #
+            line_to_write = line_to_write + ' ' + service + ' ' + status
     return line_to_write
 
 if __name__ == '__main__':
+    # Put all the services you care about in this list
     services_i_care_about = []
     print(main(services_i_care_about))
