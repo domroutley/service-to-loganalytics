c23ae903d563c586aa8b124226fcc6f74424521c
dom routley <dom@DOG00188.local>
2020-02-26 10:46:42 +0000

    convert to is-active logging Pros: - Reduces the complexity of the string manipulation - Avoids having to deal with non utf-8 characters for failiure states - Removes nested for loop Cons: - Increases the number of subprocess calls from 1 to n (where n is the number of services to check)

--- main.py
+++ main.py
@@ -1,24 +1,13 @@
 import subprocess
 import datetime
 
-def main():
-    services_i_care_about = []
+def main(services_i_care_about):
     line_to_write = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-
-    all_services = subprocess.check_output(['systemctl', '--all'])
-
-    for line in all_services.decode('utf-8').splitlines():
-        if len(services_i_care_about) == 0:
-            # We have found them all
-            break
-        for service in services_i_care_about:
-            if service in line:
-                line_to_write = line_to_write + ';' + ' '.join(line.split())
-                # Do not bother looking for this service again
-                services_i_care_about.remove(service)
-                # Stop looking through the list of services
-                break
-    print line_to_write
+    for service in services_i_care_about:
+        status = subprocess.check_output(['systemctl', 'is-active', service])
+        line_to_write = line_to_write + ' ' + service + ' ' + status
+    return line_to_write
 
 if __name__ == '__main__':
-    main()
+    services_i_care_about = []
+    print(main(services_i_care_about))
